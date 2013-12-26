9b80c4d35937a87b33833467dab81c00e4a81d2f
Chao Wang <cwang8023@gmail.com>
2013-12-26 10:23:54 +0800

    add base function for start button

--- src/com/example/android/skeletonapp/SkeletonActivity.java
+++ src/com/example/android/skeletonapp/SkeletonActivity.java
@@ -18,6 +18,10 @@ package com.example.android.skeletonapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 /**
  * This class provides a basic demonstration of how to write an Android
@@ -25,7 +29,23 @@ import android.os.Bundle;
  * displays and edits some internal text.
  */
 public class SkeletonActivity extends Activity {
-    
+
+    private ToggleButton startBtn;
+    private EditText serverAddressEditor;
+    private EditText leftSpeedPinEditor;
+    private EditText leftFrontPinEditor;
+    private EditText leftBackPinEditor;
+    private EditText rightSpeedPinEditor;
+    private EditText rightFrontPinEditor;
+    private EditText rightBackPinEditor;
+    private String serverAddress;
+    private String leftSpeedPin;
+    private String leftFrontPin;
+    private String leftBackPin;
+    private String rightSpeedPin;
+    private String rightFrontPin;
+    private String rightBackPin;
+
     public SkeletonActivity() {
     }
 
@@ -36,6 +56,66 @@ public class SkeletonActivity extends Activity {
 
         // Inflate our UI from its XML layout description.
         setContentView(R.layout.skeleton_activity);
+        initializeControls();
+        bind();
+    }
+
+    private void bind() {
+        startBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    updateConfiguration();
+                    connect();
+                }else{
+                    Toast.makeText(getBaseContext(), "Disconnected all", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void connect() {
+        connectServer();
+        connectArduino();
+    }
+
+    private void connectArduino() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Arduino Connected: ").append("\n");
+        sb.append("ea:").append(leftSpeedPin).append("\n");
+        sb.append("i1:").append(leftFrontPin).append("\n");
+        sb.append("i2:").append(leftBackPin).append("\n");
+        sb.append("eb:").append(rightSpeedPin).append("\n");
+        sb.append("i3:").append(rightFrontPin).append("\n");
+        sb.append("i4:").append(rightBackPin).append("\n");
+        Toast.makeText(getBaseContext(), sb.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void connectServer() {
+        Toast.makeText(getBaseContext(), "Server Connected: " + serverAddress, Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateConfiguration() {
+        serverAddress = serverAddressEditor.getText().toString();
+
+        leftSpeedPin = leftSpeedPinEditor.getText().toString();
+        leftFrontPin = leftFrontPinEditor.getText().toString();
+        leftBackPin = leftBackPinEditor.getText().toString();
+
+        rightSpeedPin = rightSpeedPinEditor.getText().toString();
+        rightFrontPin = rightFrontPinEditor.getText().toString();
+        rightBackPin = rightBackPinEditor.getText().toString();
+    }
+
+    private void initializeControls() {
+        startBtn = (ToggleButton) findViewById(R.id.start);
+        serverAddressEditor = (EditText) findViewById(R.id.serverAddress);
+        leftSpeedPinEditor = (EditText) findViewById(R.id.ea);
+        leftFrontPinEditor = (EditText)findViewById(R.id.i1);
+        leftBackPinEditor = (EditText)findViewById(R.id.i2);
+        rightSpeedPinEditor = (EditText)findViewById(R.id.eb);
+        rightFrontPinEditor = (EditText)findViewById(R.id.i3);
+        rightBackPinEditor = (EditText)findViewById(R.id.i4);
     }
 
     /**
